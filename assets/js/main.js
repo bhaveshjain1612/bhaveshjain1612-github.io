8aca09d44c5a55dffb26c55c634e129a29622415
Bhavesh Jain <70514193+bhaveshjain1612@users.noreply.github.com>
2021-07-25 01:54:19 +0530

    Updated main.js
    Added autoclose feature for details in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -286,6 +286,21 @@
 
 			};
 
+			// Fetch all the details element.
+				const details = document.querySelectorAll("details");
+									
+			// Add the onclick listeners.
+				details.forEach((targetDetail) => {
+					targetDetail.addEventListener("click", () => {
+												// Close all the details that are not targetDetail.
+												details.forEach((detail) => {
+												  if (detail !== targetDetail) {
+													detail.removeAttribute("open");
+												  }
+												});
+											  });
+											});
+
 		// Articles.
 			$main_articles.each(function() {
 
